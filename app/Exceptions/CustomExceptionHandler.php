9da1345b3bc05dcede68eebf2b7fd62a96617681
Tiago Gomes <tiagosilvagomes85@gmail.com>
2024-02-01 15:21:44 +0000

    In a API we require all responses to return json including the default ones, this may need to be tweaked in the future.

---
+++ app/Exceptions/CustomExceptionHandler.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+
+class CustomExceptionHandler extends ExceptionHandler
+{
+    protected $dontReport = [
+        // Add any exception classes that should not be reported to the logs here
+    ];
+
+    public function report(\Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    public function render($request, \Throwable $exception)
+    {
+        // Customize the JSON response here
+        $status = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500;
+
+        $data = [
+            'status' => $status,
+            'message' => $exception->getMessage(),
+            'data' => null,
+        ];
+
+        return new JsonResponse($data, $status);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Exceptions\CustomExceptionHandler;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->singleton(
+            \Illuminate\Contracts\Debug\ExceptionHandler::class,
+            CustomExceptionHandler::class
+        );
     }
 }
