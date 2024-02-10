df1d2fdb30759458038126e7013ae0af4724ea7c
Tiago Gomes <tiagosilvagomes85@gmail.com>
2024-02-10 18:16:28 +0000

    Update error message for validation errors in registration API

--- app/Exceptions/CustomExceptionHandler.php
+++ app/Exceptions/CustomExceptionHandler.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\JsonResponse;
+use Throwable;
 
 class CustomExceptionHandler extends ExceptionHandler
 {
@@ -10,24 +12,25 @@ class CustomExceptionHandler extends ExceptionHandler
         // Add any exception classes that should not be reported to the logs here
     ];
 
-    public function report(\Throwable $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
-    public function render($request, \Throwable $exception)
+    public function render($request, Throwable $exception)
     {
         // Customize the JSON response here
         $status = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 0;
-        if($status==0 || $status == '') {
-            $status = isset($exception->status) ?
-            $exception->status :
-            500;
+
+        if ($status == 0 || $status == '') {
+            $status = isset($exception->status) ? $exception->status : 500;
         }
+
+        $customMessageMethod = 'getCustomMessage';
 
         $data = [
             'status' => $status,
-            'message' => $exception->getMessage(),
+            'message' => method_exists($exception, $customMessageMethod) ? $exception->$customMessageMethod() : $exception->getMessage(),
             'data' => null,
         ];
 
