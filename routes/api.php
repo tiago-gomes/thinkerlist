91310e3488303af57e2bce4ce3a516b7c11d9471
Tiago Gomes <tiagosilvagomes85@gmail.com>
2024-10-17 11:12:53 +0100

    removing unrequired routes

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,3 @@ use App\Http\Controllers\V1\Schedule\ScheduleRuleController;
 use Illuminate\Support\Facades\Route;
 
 // Public Routes
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-
-// Routes that require authentication
-Route::middleware('auth:sanctum')->group(function () {
-
-    // schedule rules
-    Route::get('/schedule-rules', [ScheduleRuleController::class, 'index'])->name('schedule.rules.index');
-    Route::post('/schedule-rules', [ScheduleRuleController::class, 'store'])->name('schedule.rules.store');
-    Route::patch('/schedule-rules/{id}', [ScheduleRuleController::class, 'update'])->name('schedule.rules.update');
-
-    // authentication
-    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-});
